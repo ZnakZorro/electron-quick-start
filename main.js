a09d66ba0653b8a1795c82855e179f5d364fc34a
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-11-21 12:21:01 +0100

    add menu

--- main.js
+++ main.js
@@ -1,17 +1,141 @@
+const electron = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
+
+
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+//const {app, BrowserWindow} = require('electron')
 //require('electron-debug')();
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 886, height: 680})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
+  
+  
+  
+ /********************/
+
+//https://electronjs.org/docs/api/menu-item
+
+              var application_menu = [
+                {
+                  label: 'radioMenu',
+                  submenu: [
+                    {
+                      label: 'Reload',
+                      accelerator: 'CmdOrCtrl+R',
+                      role: 'forceReload'
+                    },
+                    {
+                      label: 'Undo',
+                      accelerator: 'CmdOrCtrl+Z',
+                      role: 'undo'
+                    },
+                    {
+                      label: 'Open',
+                      accelerator: 'CmdOrCtrl+O',
+                      click: () => {
+                        electron.dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory', 'multiSelections' ]});
+                      }
+                    },
+                    {
+                      label: 'DevTools',
+                      submenu: [
+                        {
+                          label: 'openDev',
+                          accelerator: 'CmdOrCtrl+A',
+                          click: () => {
+                            mainWindow.openDevTools();
+                          }
+                        },
+                        {
+                          label: 'CloseDev',
+                          accelerator: 'CmdOrCtrl+B',
+                          click: () => {
+                            mainWindow.closeDevTools();
+                          }
+                        }
+                      ]
+                    },
+                    {
+                      label: 'Exit',
+                      accelerator: 'CmdOrCtrl+Q',
+                      role: 'quit'
+                    }
+                  ]
+                }
+              ];
+/*              
+              if (process.platform == 'darwin') {
+                const name = app.getName();
+                application_menu.unshift({
+                  label: name,
+                  submenu: [
+                    {
+                      label: 'About ' + name,
+                      role: 'about'
+                    },
+                    {
+                      type: 'separator'
+                    },
+                    {
+                      label: 'Services',
+                      role: 'services',
+                      submenu: []
+                    },
+                    {
+                      type: 'separator'
+                    },
+                    {
+                      label: 'Hide ' + name,
+                      accelerator: 'Command+H',
+                      role: 'hide'
+                    },
+                    {
+                      label: 'Hide Others',
+                      accelerator: 'Command+Shift+H',
+                      role: 'hideothers'
+                    },
+                    {
+                      label: 'Show All',
+                      role: 'unhide'
+                    },
+                    {
+                      type: 'separator'
+                    },
+                    {
+                      label: 'Quit',
+                      accelerator: 'Command+Q',
+                      click: () => { app.quit(); }
+                    },
+                  ]
+                });
+              }
+
+ */
+ menu = Menu.buildFromTemplate(application_menu);
+              Menu.setApplicationMenu(menu);
+
+
+
+/*****************************/ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
